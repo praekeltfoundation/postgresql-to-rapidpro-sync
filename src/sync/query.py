c1052177a183d2b010d79436562286b18f85025b
Rudi Giesler <rudi@praekelt.org>
2022-05-24 12:00:32 +0200

    Add helper for getting all rows from the table as dictionaries

--- src/sync/query.py
+++ src/sync/query.py
@@ -1,5 +1,26 @@
+from typing import Any, AsyncIterator
+
+from psycopg import AsyncConnection
 from psycopg.sql import SQL, Composed, Identifier
 
 
 def get_all_rows_query(table: str) -> Composed:
+    """
+    SQL query for getting all rows from the table
+    """
     return SQL("SELECT * FROM {table}").format(table=Identifier(table))
+
+
+async def get_all_rows(
+    connection: AsyncConnection, table: str
+) -> AsyncIterator[dict[str, Any]]:
+    """
+    Async iterator that returns all the rows of the table, as dictionaries
+    """
+    async with connection.cursor() as cursor:
+        await cursor.execute(get_all_rows_query(table))
+        if cursor.description is None:
+            raise ValueError("No column description in cursor")
+        columns = [col.name for col in cursor.description]
+        async for record in cursor:
+            yield dict(zip(columns, record))
--- tests/test_query.py
+++ tests/test_query.py
@@ -4,18 +4,55 @@ from typing import AsyncGenerator
 from psycopg import AsyncConnection
 from pytest import fixture
 
-from sync.query import get_all_rows_query
+from sync.query import get_all_rows, get_all_rows_query
 
 
 @fixture
 async def database_connection() -> AsyncGenerator[AsyncConnection, None]:
     async with await AsyncConnection.connect(environ.get("DATABASE_DSN", "")) as aconn:
         yield aconn
+        await aconn.rollback()
 
 
-async def test_get_all_rows(database_connection: AsyncConnection):
+async def create_test_database_data(database_connection: AsyncConnection) -> None:
+    async with database_connection.cursor() as cursor:
+        await cursor.execute(
+            """
+        CREATE TABLE test_table (
+            whatsapp varchar(20),
+            field1 varchar(20),
+            field2 varchar(20)
+        )
+        """
+        )
+        await cursor.execute(
+            """
+        INSERT INTO test_table(whatsapp, field1, field2) VALUES
+            ('27820001001', 'c1f1', 'c1f2'),
+            ('27820001002', 'c2f1', 'c2f2')
+        """
+        )
+
+
+async def test_get_all_rows_query(database_connection: AsyncConnection):
+    """
+    Properly escapes the table name in the query
+    """
     query = get_all_rows_query("test_table")
     assert query.as_string(database_connection) == 'SELECT * FROM "test_table"'
 
     query = get_all_rows_query('test " \0 ')
     assert query.as_string(database_connection) == 'SELECT * FROM "test "" "'
+
+
+async def test_get_all_rows(database_connection: AsyncConnection):
+    """
+    Fetches all the rows from the table, and returns them as dictionaries
+    """
+    await create_test_database_data(database_connection)
+
+    rows = [r async for r in get_all_rows(database_connection, "test_table")]
+    assert sorted(rows, key=lambda r: r["whatsapp"]) == [
+        {"field1": "c1f1", "field2": "c1f2", "whatsapp": "27820001001"},
+        {"field1": "c2f1", "field2": "c2f2", "whatsapp": "27820001002"},
+    ]
